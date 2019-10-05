2d437c4ea180b607eca4be271f8b3e1dd55110a5
AZagatti <andre.zagatti1@gmail.com>
2019-10-05 20:01:25 -0300

    Create Spot

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.store);
+routes.post('/spots', SpotController.store);
 
 module.exports = routes;
