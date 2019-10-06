5e587fa5e29c135c54004b304d0cf3b30ede306b
AZagatti <andre.zagatti1@gmail.com>
2019-10-06 16:38:26 -0300

    Change backend and finished frontend React

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -13,7 +15,9 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
