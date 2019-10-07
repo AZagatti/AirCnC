6a25004b8c23cffedfc6fa99c8351d87fefe0fbe
AZagatti <andre.zagatti1@gmail.com>
2019-10-07 00:01:50 -0300

    Start implements Web Socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@semanaomnistack09-ycnoo.mongodb.net/semana09?retryWrites=true&w=majority',
@@ -15,9 +20,24 @@ mongoose.connect(
   }
 );
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
